9397bdf4b98452b5157a656264fb29169cf03851
symonk <jackofspaces@gmail.com>
2020-05-29 16:06:39 +0100

    tidy up imports

--- src/browzer/__init__.py
+++ src/browzer/__init__.py
@@ -1,6 +1,8 @@
 __version__ = "0.0.0"
 
-from browzer.configuration.BrowzerConfig import BrowzerConfiguration
+from browzer.configuration.BrowzerConfig import load_browzer_config
+from browzer.configuration.BrowzerConfig import BrowzerConfiguration  # noqa: F401
 
+browzer_config = load_browzer_config()
 
-__all__ = [BrowzerConfiguration]
+__all__ = [browzer_config]
