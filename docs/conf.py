66f5d6b3114d33b738770dfe7bdb1408196922fb
symonk <jackofspaces@gmail.com>
2020-08-03 21:19:07 +0100

    update docs theme to rtd

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
-import sphinx_py3doc_enhanced_theme
+import sphinx_rtd_theme
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -13,7 +12,9 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinx_rtd_theme",
 ]
+
 source_suffix = ".rst"
 master_doc = "index"
 project = "Browzer"
@@ -28,8 +29,7 @@ extlinks = {
     "issue": ("https://github.com/symonk/browzer/issues/%s", "#"),
     "pr": ("https://github.com/symonk/browzer/pull/%s", "PR #"),
 }
-html_theme = "sphinx_py3doc_enhanced_theme"
-html_theme_path = [sphinx_py3doc_enhanced_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
 html_theme_options = {"githuburl": "https://github.com/symonk/browzer/"}
 
 html_use_smartypants = True
