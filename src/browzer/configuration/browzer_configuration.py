1ce7a849e9c9a5fb4dae71326dc21d87d6369fd6
symonk <jackofspaces@gmail.com>
2020-08-06 22:51:06 +0100

    more properties for config values

--- src/browzer/configuration/browzer_configuration.py
+++ src/browzer/configuration/browzer_configuration.py
@@ -51,21 +51,21 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         self._selenium_grid_url: str = selenium_grid_url
         self._selenium_grid_port: int = selenium_grid_port
         self._browser_resolution: str = browser_resolution
-        self.browser_version: str = browser_version
-        self.driver_binary_path: str = driver_binary_path
-        self.browser_capabilities: Dict[str, str] = browser_capabilities
-        self.chrome_options: List[str] = chrome_options
-        self.base_url: str = base_url
-        self.explicit_waiting: float = explicit_waiting
-        self.polling_interval: float = polling_interval
-        self.page_source_capturing: bool = page_source_capturing
-        self.page_screenshot_capturing: bool = page_screenshot_capturing
-        self.stack_trace_capturing: bool = stack_trace_capturing
-        self.javascript_clicks: bool = javascript_clicks
-        self.javascript_sendkeys: bool = javascript_sendkeys
-        self.driver_listener_module_class_path: str = driver_listener_module_class_path
-        self.page_loading_strategy: str = page_loading_strategy
-        self.default_selector: str = default_selector
+        self._browser_version: str = browser_version
+        self._driver_binary_path: str = driver_binary_path
+        self._browser_capabilities: Dict[str, str] = browser_capabilities
+        self._chrome_options: List[str] = chrome_options
+        self._base_url: str = base_url
+        self._explicit_waiting: float = explicit_waiting
+        self._polling_interval: float = polling_interval
+        self._page_source_capturing: bool = page_source_capturing
+        self._page_screenshot_capturing: bool = page_screenshot_capturing
+        self._stack_trace_capturing: bool = stack_trace_capturing
+        self._javascript_clicks: bool = javascript_clicks
+        self._javascript_sendkeys: bool = javascript_sendkeys
+        self._driver_listener_module_class_path: str = driver_listener_module_class_path
+        self._page_loading_strategy: str = page_loading_strategy
+        self._default_selector: str = default_selector
 
     @property
     def browser(self) -> str:
@@ -76,16 +76,16 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         return self._browser
 
     @browser.setter
-    def browser(self, value: str) -> None:
+    def browser(self, browser: str) -> None:
         """
         Setter for the browser attribute.
         :param value: The browser to configure
         """
         supported = {"chrome", "firefox"}
         enforce_value_is_in(
-            iterable=supported, value=value, exc=BrowzerConfigValueError
+            iterable=supported, value=browser.lower(), exc=BrowzerConfigValueError
         )
-        self._browser = value.lower()
+        self._browser = browser.lower()
 
     @property
     def headless(self) -> bool:
@@ -96,15 +96,15 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         return self._headless
 
     @headless.setter
-    def headless(self, value: bool) -> None:
+    def headless(self, headless: bool) -> None:
         """
         The setter for the headless attribute
         """
-        message = f"Only boolean types are supported for headless, you provided: {type(value)}"
+        message = f"Only boolean types are supported for headless, you provided: {type(headless)}"
         enforce_type_of(
-            expected=bool, value=value, exc=BrowzerConfigValueError, msg=message
+            expected=bool, value=headless, exc=BrowzerConfigValueError, msg=message
         )
-        self._headless = value
+        self._headless = headless
 
     @property
     def remote(self) -> bool:
@@ -115,17 +115,17 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         return self._remote
 
     @remote.setter
-    def remote(self, value: bool) -> None:
+    def remote(self, remote: bool) -> None:
         """
         The setter for the remote attribute
         """
         message = (
-            f"Only boolean types are supported for remote, you provided: {type(value)}"
+            f"Only boolean types are supported for remote, you provided: {type(remote)}"
         )
         enforce_type_of(
-            expected=bool, value=value, exc=BrowzerConfigValueError, msg=message
+            expected=bool, value=remote, exc=BrowzerConfigValueError, msg=message
         )
-        self._remote = value
+        self._remote = remote
 
     @property
     def selenium_grid_url(self) -> str:
@@ -136,16 +136,16 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         return self._selenium_grid_url
 
     @selenium_grid_url.setter
-    def selenium_grid_url(self, value: str) -> None:
+    def selenium_grid_url(self, selenium_grid_url: str) -> None:
         """
         The setter for the selenium_grid_url attribute
         """
         # TODO -> Better validation, do not end with /wd/hub we append it etc
-        message = f"selenium grid url must be a string, but you provided: {type(value)}"
+        message = f"selenium grid url must be a string, but you provided: {type(selenium_grid_url)}"
         enforce_type_of(
-            expected=str, value=value, exc=BrowzerConfigValueError, msg=message
+            expected=str, value=selenium_grid_url, exc=BrowzerConfigValueError, msg=message
         )
-        self._selenium_grid_url = value
+        self._selenium_grid_url = selenium_grid_url
 
     @property
     def selenium_grid_port(self) -> int:
@@ -156,17 +156,17 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         return self._selenium_grid_port
 
     @selenium_grid_port.setter
-    def selenium_grid_port(self, value: int) -> None:
+    def selenium_grid_port(self, selenium_grid_port: int) -> None:
         """
         The setter for the selenium_grid_port attribute
         """
         message = (
-            f"selenium grid url must be an integer, but you provided: {type(value)}"
+            f"selenium grid url must be an integer, but you provided: {type(selenium_grid_port)}"
         )
         enforce_type_of(
-            expected=int, value=value, exc=BrowzerConfigValueError, msg=message
+            expected=int, value=selenium_grid_port, exc=BrowzerConfigValueError, msg=message
         )
-        self._selenium_grid_port = value
+        self._selenium_grid_port = selenium_grid_port
 
     @property
     def browser_resolution(self) -> str:
@@ -176,18 +176,90 @@ class BrowzerConfiguration(SimpleReprMixin, SimpleEQMixing):
         return self._browser_resolution
 
     @browser_resolution.setter
-    def browser_resolution(self, value: str) -> None:
+    def browser_resolution(self, browser_resolution: str) -> None:
         """
         The setter for the browser_resolution attribute
         """
         # TODO -> Better validation on a intXint
         message = (
-            f"browser_resolution must be a string, but you provided: {type(value)}"
+            f"browser_resolution must be a string, but you provided: {type(browser_resolution)}"
         )
         enforce_type_of(
-            expected=str, value=value, exc=BrowzerConfigValueError, msg=message
+            expected=str, value=browser_resolution, exc=BrowzerConfigValueError, msg=message
         )
-        self._selenium_grid_url = value
+        self._selenium_grid_url = browser_resolution
+
+    @property
+    def browser_version(self) -> str:
+        """
+        Getter for the browser_version attribute
+        """
+        return self._browser_version
+
+    @browser_version.setter
+    def browser_version(self, version: str) -> None:
+        """
+        Setter for the browser_version attribute
+        """
+        message = (
+            f"browser_version must be a string, but you provided: {type(version)}"
+        )
+        enforce_type_of(
+            expected=str, value=version, exc=BrowzerConfigValueError, msg=message
+        )
+        self._browser_version = version
+
+    @property
+    def driver_binary_path(self) -> str:
+        """
+        Getter for the driver_binary_path
+        """
+        return self._driver_binary_path
+
+    @driver_binary_path.setter
+    def driver_binary_path(self, binary_path: str) -> None:
+        """
+        Setter for the driver_binary_path
+        This is used to determine which version of the binaries to automatically acquire.
+        If this value is 'latest', we will automatically acquire the latest version for the given binary.
+        """
+        self._driver_binary_path = binary_path
+
+    @property
+    def browser_capabilities(self) -> Dict[str, str]:
+        """
+        Getter for the browser_capabilities attribute
+        """
+        return self._browser_capabilities
+
+    @browser_capabilities.setter
+    def browser_capabilities(self, capabilities: Dict[str, str]) -> None:
+        """
+        Setter for the browser_capabilities attribute
+        """
+        message = (
+            f"browser_capabilities must be a dictionary, but you provided: {type(capabilities)}"
+        )
+        enforce_type_of(
+            expected=Dict, value=capabilities, exc=BrowzerConfigValueError, msg=message
+        )
+        self._browser_capabilities = capabilities
+
+    @property
+    def chrome_options(self) -> List[str]:
+        """
+        Getter for the chrome_options attribute
+        """
+        return self._chrome_options
+
+    @chrome_options.setter
+    def chrome_options(self, chrome_options: List[str]) -> None:
+        """
+        Setter for the chrome_options attribute
+        """
+        message = f"chrome_options must be a list, but you provided: {type(chrome_options)})"
+        enforce_type_of(expected=List, value=chrome_options, exc=BrowzerConfigValueError, msg=message)
+        self._chrome_options = chrome_options
 
     @property
     def full_hub_endpoint(self) -> str:
