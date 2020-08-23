bb7735751d3605703d28558681d89191e5c90e97
symonk <jackofspaces@gmail.com>
2020-08-23 11:49:29 +0100

    improve the EQ mixin

--- src/sylenium/mixins/simple_eq_mixin.py
+++ src/sylenium/mixins/simple_eq_mixin.py
@@ -4,4 +4,4 @@ class SimpleEQMixin:
     """
 
     def __eq__(self, other) -> bool:
-        return self == other
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
