7660afd2af88aa055f76007b37f5d0d9f672d516
symonk <jackofspaces@gmail.com>
2020-08-23 11:55:49 +0100

    use type checks instead of instance

--- src/sylenium/mixins/simple_eq_mixin.py
+++ src/sylenium/mixins/simple_eq_mixin.py
@@ -4,4 +4,4 @@ class SimpleEQMixin:
     """
 
     def __eq__(self, other) -> bool:
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+        return type(other) is type(self) and self.__dict__ == other.__dict__
