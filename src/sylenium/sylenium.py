2c380d3c0d7491845535a49797c469d1977c9e90
symonk <jackofspaces@gmail.com>
2020-08-31 14:21:25 +0100

    use actual Session types

--- src/sylenium/sylenium.py
+++ src/sylenium/sylenium.py
@@ -4,14 +4,14 @@ from typing import Mapping
 
 from sylenium.core.core import Session
 
-SESSIONS: Mapping[str, "Session"] = {}
+SESSIONS: Mapping[str, Session] = {}
 
 
 def get_session_information():
     ...
 
 
-def register_session(session: "Session"):
+def register_session(session: Session):
     global SESSIONS
     unique_id = threading.get_ident()
     session[unique_id] = session
