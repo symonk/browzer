c2fb213333be12775894206524d6c67722e9a74d
symonk <jackofspaces@gmail.com>
2020-09-01 10:10:31 +0100

    test reshuffling

---
+++ tests/integration/webdriver/downloads/test_downloads.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+from assertpy import assert_that
+
+from sylenium import Configuration
+
+
+def test_download_file(sy_session, tmpdir):
+    with sy_session(Configuration(downloads_directory=tmpdir)) as session:
+        session.get_driver().get("http://ipv4.download.thinkbroadband.com/1MB.zip")
+        # TODO => Add a wait_until method or library and wait until .crdownload is no more.
+        time.sleep(5)
+        assert_that(os.listdir(tmpdir)).contains("1MB.zip")
--- tests/unit_tests/configuration/test_configuration_obj.py
+++ tests/unit_tests/configuration/test_configuration_obj.py
@@ -1,6 +1,3 @@
-import os
-import time
-
 from assertpy import assert_that
 
 from sylenium.configuration import Configuration
@@ -8,11 +5,3 @@ from sylenium.configuration import Configuration
 
 def test_sylenium_config_load(default_session):
     assert_that(default_session.config).is_instance_of(Configuration)
-
-
-def test_download_file(sy_session, tmpdir):
-    with sy_session(Configuration(downloads_directory=tmpdir)) as session:
-        session.get_driver().get("http://ipv4.download.thinkbroadband.com/1MB.zip")
-        # TODO => Add a wait_until method or library and wait until .crdownload is no more.
-        time.sleep(5)
-        assert_that(os.listdir(tmpdir)).contains("1MB.zip")
