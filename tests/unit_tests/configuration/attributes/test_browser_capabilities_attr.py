61627e038274aebe6aa0f72661d227a0da21b0d9
symonk <jackofspaces@gmail.com>
2020-09-09 10:21:01 +0100

    unit tests for browser capabilities dictionary

--- tests/unit_tests/configuration/attributes/test_browser_capabilities_attr.py
+++ tests/unit_tests/configuration/attributes/test_browser_capabilities_attr.py
@@ -1,14 +1,21 @@
+import pytest
+from assertpy import assert_that
+
+
 def test_browser_capabilities_default(configuration):
-    ...
+    assert_that(configuration().browser_capabilities).is_none()
 
 
 def test_browser_capabilities_custom(configuration):
-    ...
+    dictionary = {"a": "one", "b": "two"}
+    assert_that(
+        configuration(browser_capabilities=dictionary).browser_capabilities
+    ).is_equal_to(dictionary)
 
 
 def test_browser_capabilities_incorrect_type(configuration):
-    ...
-
-
-def test_browser_capabilities_using_direct_capabilities(configuration):
-    ...
+    with pytest.raises(TypeError) as error:
+        configuration(browser_capabilities=1337)
+    assert_that(error.value.args[0]).is_equal_to(
+        "browser_capabilities= should be of type: <class 'dict'>"
+    )
