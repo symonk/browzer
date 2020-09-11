44b430239353c7c919061aa9a613a2dd503a65b1
symonk <jackofspaces@gmail.com>
2020-09-11 23:47:54 +0100

    fix tests

--- tests/integration/webdriver/downloads/test_downloads.py
+++ tests/integration/webdriver/downloads/test_downloads.py
@@ -6,9 +6,9 @@ from assertpy import assert_that
 from sylenium import Configuration
 
 
-def test_download_file(sy_session, tmpdir) -> None:
-    with sy_session(Configuration(download_directory=str(tmpdir))) as session:
-        session.get_driver().get("http://ipv4.download.thinkbroadband.com/1MB.zip")
+def test_download_file(driver_creator, tmpdir) -> None:
+    with driver_creator(Configuration(download_directory=str(tmpdir))) as driver:
+        driver.get("http://ipv4.download.thinkbroadband.com/1MB.zip")
         # TODO => Add a wait_until method or library and wait until .crdownload is no more.
         time.sleep(5)
         assert_that(os.listdir(tmpdir)).contains("1MB.zip")
