2425eff5ff3eb1e973591c96acf11f5671b34575
symonk <jackofspaces@gmail.com>
2020-09-13 16:57:43 +0100

    documenting some should methods

--- src/sylenium/element/sylenium_element.py
+++ src/sylenium/element/sylenium_element.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from selenium.webdriver.remote.webelement import WebElement as RemoteWebElement
 
 from sylenium.interface.locatable import Locatable
@@ -7,3 +9,21 @@ class SyleniumElement:
     def __init__(self, delegated_element: RemoteWebElement, locatable: Locatable):
         self.element = delegated_element
         self.locatable = locatable
+
+    def should_be(self) -> SyleniumElement:
+        ...
+
+    def should_have(self) -> SyleniumElement:
+        ...
+
+    def should_not_be(self) -> SyleniumElement:
+        ...
+
+    def should_not_have(self) -> SyleniumElement:
+        ...
+
+    def wait_until(self) -> SyleniumElement:
+        ...
+
+    def wait_while(self) -> SyleniumElement:
+        ...
